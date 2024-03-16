422be1b27b1613a85e57a59acfece8946a2b9b03
BAB AL SAFA <emanmohamedmahmoud789@gmail.com>
2024-03-16 11:19:56 +0200

    Splash screen and used image resources

---
+++ app/src/main/java/com/example/newsapp/Screen.kt
@@ -0,0 +1,14 @@
+package com.example.newsapp
+
+
+object NewsFragmentScreen {
+    val ROUTE_NAME = "news"
+}
+
+object CategoriesFragmentScreen {
+    val ROUTE_NAME = "categories"
+}
+
+object SettingsFragmentScreen {
+    val ROUTE_NAME = "settings"
+}
